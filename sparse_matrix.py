a31e59d741d943ea97b97578c0d4045a95014da5
Nguyễn Văn Anh Tuấn <nvatuan3@gmail.com>
2021-09-22 15:38:37 +0700

    OK

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -2,16 +2,11 @@ from typing import List, Dict
 
 
 class SparseMatrix(object):
-    def __init__(self, matrix, n: int = None, m: int = None):
-        if type(matrix) == list:  # if matrix is list like
-            self.dense_ = matrix
-            self.n = len(matrix)
-            self.m = len(matrix[0])
-            self.sparse_ = self.__to_sparse()
-        else:  # if matrix is a dictionary
-            self.sparse_ = matrix
-            self.n = n
-            self.m = m
+    def __init__(self, matrix: List[List]):
+        self.dense_ = matrix
+        self.n = len(matrix)
+        self.m = len(matrix[0])
+        self.sparse_ = self.__to_sparse()
 
     def __to_sparse(self) -> Dict:
         sparse_matrix = {}
@@ -31,63 +26,55 @@ class SparseMatrix(object):
         if self.n != obj.n or self.m != obj.m:
             raise Exception("Không cùng kích cỡ")
 
-        A = self.sparse_
-        B = obj.sparse_
+        A = self.to_dense()
+        B = obj.to_dense()
         for i in range(self.n):
             for j in range(self.m):
-                try:
-                    A[(i, j)] = A[(i, j)] + B[(i, j)]
-                except:
-                    pass
-        return SparseMatrix(A, self.n, self.m)
+                A[i][j] += B[i][j]
+        return SparseMatrix(A)
 
     def __sub__(self, obj: object):
         if self.n != obj.n or self.m != obj.m:
             raise Exception("Không cùng kích cỡ")
 
-        A = self.sparse_
-        B = obj.sparse_
+        A = self.to_dense()
+        B = obj.to_dense()
         for i in range(self.n):
             for j in range(self.m):
-                try:
-                    A[(i, j)] = A[(i, j)] - B[(i, j)]
-                except:
-                    pass
-        return SparseMatrix(A, self.n, self.m)
+                A[i][j] -= B[i][j]
+        return SparseMatrix(A)
 
     def __mul__(self, obj: object):
         if self.n != obj.n or self.m != obj.m:
             raise Exception("Không cùng kích cỡ")
 
-        A = self.sparse_
-        B = obj.sparse_
+        A = self.to_dense()
+        B = obj.to_dense()
         for i in range(self.n):
             for j in range(self.m):
-                try:
-                    A[(i, j)] = A[(i, j)] * B[(i, j)]
-                except:
-                    pass
-        return SparseMatrix(A, self.n, self.m)
+                A[i][j] *= B[i][j]
+        return SparseMatrix(A)
 
     def __matmul__(self, obj: object):
         if self.m != obj.n:
             raise Exception("Không cùng kích cỡ để nhân ma trận")
 
-        A = self.sparse_
-        B = obj.sparse_
-        C = {}
+        A = self.to_dense()
+        B = obj.to_dense()
+        C = [[0 for i in range(obj.m)] for j in range(self.n)]
         for i in range(self.n):
             for j in range(self.m):
                 for k in range(obj.m):
-                    try:
-                        C[(i, k)] += A[(i, j)] * B[(j, k)]
-                    except:
-                        pass
-        return SparseMatrix(C, self.n, obj.m)
+                    C[i][k] += A[i][j] * B[j][k]
+        return SparseMatrix(C)
 
     def transpose(self):
-        A = {(j, i): val for (i, j), val in self.sparse_.items()}
-        return SparseMatrix(A, self.m, self.n)
+        A = self.to_dense()
+        B = [[0 for i in range(self.n)] for j in range(self.m)]
+        for i in range(self.n):
+            for j in range(self.m):
+                B[j][i] = A[i][j]
+        return SparseMatrix(B)
 
     def __repr__(self):
         return '<Sparse Matrix {0.n}x{0.m}>'.format(self)
